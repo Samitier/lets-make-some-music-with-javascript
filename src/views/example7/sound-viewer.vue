a99cded5faf78f567d175c3c312083fcd761345a
Blai Samitier <blai@samitier.me>
2018-10-15 20:51:28 +0200

    multiple bananas on sound viewer

--- src/views/example7/sound-viewer.vue
+++ src/views/example7/sound-viewer.vue
@@ -1,6 +1,11 @@
 <template>
 	<div class="sound-viewer row" :style="cssStyle">
-		<img :class="cssClass" src="/img/banana.gif">
+		<img 
+			v-for="banana in bananas" 
+			:key="banana.key"
+			:style="banana" 
+			src="/img/banana.gif"
+		>
 	</div>
 </template>
 
@@ -11,39 +16,56 @@ import randomcolor from "randomcolor"
 @Component
 export default class SoundViewer extends Vue {
 
-	get cssStyle() { return { backgroundColor: this.color } }
-	get cssClass() { return this.isVisible && "visible" }
+	bananas: Banana[] = []
 
-	private isVisible = false
+	get cssStyle() { return { backgroundColor: this.color } }
+
 	private color = randomcolor()
-	private timeout!: number
+	private i = 0
 
 	show() {
-		if (this.timeout) clearTimeout(this.timeout)
-		this.isVisible = true
 		this.color = randomcolor()
-		this.timeout = setTimeout(() => this.isVisible = false, 400)
+		const banana = {
+			key: this.i++,
+			top: `${ Math.random() * (window.innerHeight - 250) }px`,
+			left: `${ Math.random() * (window.innerWidth - 250) }px`,
+			transform: `rotateZ(${ 360 * Math.random() }deg) scale(${ .8 + .5 * Math.random() })`
+		}
+		this.bananas.push(banana)
+		setTimeout(() => {
+			const i = this.bananas.indexOf(banana)
+			this.bananas.splice(i, 1)
+		}, 1000)
 	}
+}
+
+interface Banana {
+	key: number
+	top: string
+	left: string
+	transform: string
 }
 </script>
 
 <style scoped lang="stylus">
+@keyframes fade
+	0%
+		opacity 0
+	10% 
+		opacity 1
+	60% 
+		opacity 1
+	100%
+		opacity 0
+
 .sound-viewer
 	top 0
 	bottom 0
 	left 0
 	right 0
 	position fixed
-	text-align center
-	font-size 180px
-	font-weight bold
-	transition all .3s
-	display flex
-	align-items center
-	justify-content center
-	>*
-		opacity 0
-		transition opacity 0.3s
-.visible
-	opacity 1
+
+img
+	position absolute
+	animation fade 1000ms forwards
 </style>
